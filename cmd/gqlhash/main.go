55239ba68ab016f3044059bc2c6cd77c883ad37c
Roman Sharkov <roman.scharkov@gmail.com>
2024-11-03 19:18:17 +0100

    feat: Add hash functions
    Add sha2, sha3, blake2b, blake2s, fnv, crc32, crc64

--- cmd/gqlhash/main.go
+++ cmd/gqlhash/main.go
@@ -1,21 +1,36 @@
 package main
 
 import (
+	"crypto/md5"
 	"crypto/sha1"
+	"crypto/sha256"
 	"debug/buildinfo"
 	"encoding/base64"
 	"encoding/hex"
 	"flag"
 	"fmt"
+	"hash"
+	"hash/crc32"
+	"hash/crc64"
+	"hash/fnv"
 	"io"
 	"os"
 	"os/exec"
 	"strings"
 
+	"golang.org/x/crypto/blake2b"
+	"golang.org/x/crypto/blake2s"
+	"golang.org/x/crypto/sha3"
+
 	"github.com/romshark/gqlhash"
 )
 
-const Version = `1.0.0`
+const (
+	Version                = `1.1.0`
+	SupportedHashFunctions = "sha1, sha2, sha3, md5, blake2b, blake2s, " +
+		"fnv, crc32, crc64"
+	SupportedOutputFormats = "hex, base64"
+)
 
 func main() {
 	fFile := flag.String(
@@ -26,7 +41,19 @@ func main() {
 	fFormat := flag.String(
 		"format",
 		"hex",
-		`Hash format (hex, base64)`,
+		"Hash format ("+SupportedOutputFormats+")",
+	)
+	fHashFunction := flag.String(
+		"hash",
+		"sha1",
+		"Selects the hash function "+
+			"("+SupportedHashFunctions+").\n"+
+			"sha2 is SHA-256.\n"+
+			"sha3 is SHA3-512.\n"+
+			"blake2b is unkeyed.\n"+
+			"blake2s is unkeyed.\n"+
+			"crc32 uses the IEEE polynomial.\n"+
+			"crc64 uses ISO polynomial, defined in ISO 3309 and used in HDLC.",
 	)
 	fVersion := flag.Bool(
 		"version",
@@ -39,9 +66,20 @@ func main() {
 		PrintVersionInfoAndExit()
 	}
 
-	if !strings.EqualFold(*fFormat, "hex") &&
-		!strings.EqualFold(*fFormat, "base64") {
-		fmt.Fprint(os.Stderr, "unsupported format, use any of: hex, base64")
+	if !isValidOutputFormatName(*fFormat) {
+		fmt.Fprintf(
+			os.Stderr, "unsupported format %q, use any of: "+
+				SupportedOutputFormats+"\n",
+			*fFormat,
+		)
+		os.Exit(1)
+	}
+	if !isValidHashFuncName(*fHashFunction) {
+		fmt.Fprintf(
+			os.Stderr, "unsupported hash function %q, use any of: "+
+				SupportedHashFunctions+"\n",
+			*fHashFunction,
+		)
 		os.Exit(1)
 	}
 
@@ -65,7 +103,37 @@ func main() {
 		os.Exit(1)
 	}
 
-	sum, err := gqlhash.AppendQueryHash(nil, sha1.New(), input)
+	var hasher hash.Hash
+	switch name := *fHashFunction; {
+	case strings.EqualFold(name, "sha1"):
+		hasher = sha1.New()
+	case strings.EqualFold(name, "sha2"):
+		hasher = sha256.New()
+	case strings.EqualFold(name, "sha3"):
+		hasher = sha3.New512()
+	case strings.EqualFold(name, "md5"):
+		hasher = md5.New()
+	case strings.EqualFold(name, "blake2b"):
+		hasher, err = blake2b.New256(nil)
+		if err != nil {
+			panic(fmt.Errorf("initializing blake2b hasher: %w", err))
+		}
+	case strings.EqualFold(name, "blake2s"):
+		hasher, err = blake2s.New256(nil)
+		if err != nil {
+			panic(fmt.Errorf("initializing blake2s hasher: %w", err))
+		}
+	case strings.EqualFold(name, "fnv"):
+		hasher = fnv.New64()
+	case strings.EqualFold(name, "crc32"):
+		hasher = crc32.NewIEEE()
+	case strings.EqualFold(name, "crc64"):
+		hasher = crc64.New(crc64.MakeTable(crc64.ISO))
+	default:
+		panic(fmt.Errorf("unsupported hash function: %q", name))
+	}
+
+	sum, err := gqlhash.AppendQueryHash(nil, hasher, input)
 	if err != nil {
 		panic(err)
 	}
@@ -99,8 +167,25 @@ func PrintVersionInfoAndExit() {
 
 	fmt.Printf("gqlhash v%s\n\n", Version)
 	fmt.Println("MIT License")
-	fmt.Print("Copyright (c) 2024 Roman Sharkov\n\n")
+	fmt.Print("Copyright (c) 2024 Roman Scharkov (github.com/romshark)\n\n")
 	fmt.Printf("%v\n", info)
 
 	os.Exit(0)
+}
+
+func isValidOutputFormatName(s string) bool {
+	return strings.EqualFold(s, "hex") ||
+		strings.EqualFold(s, "base64")
+}
+
+func isValidHashFuncName(s string) bool {
+	return strings.EqualFold(s, "sha1") ||
+		strings.EqualFold(s, "sha2") ||
+		strings.EqualFold(s, "sha3") ||
+		strings.EqualFold(s, "md5") ||
+		strings.EqualFold(s, "blake2b") ||
+		strings.EqualFold(s, "blake2s") ||
+		strings.EqualFold(s, "fnv") ||
+		strings.EqualFold(s, "crc32") ||
+		strings.EqualFold(s, "crc64")
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,12 @@ module github.com/romshark/gqlhash
 
 go 1.23.2
 
-require github.com/vektah/gqlparser/v2 v2.5.18
+require (
+	github.com/vektah/gqlparser/v2 v2.5.18
+	golang.org/x/crypto v0.28.0
+)
 
-require github.com/agnivade/levenshtein v1.1.1 // indirect
+require (
+	github.com/agnivade/levenshtein v1.1.1 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+)
