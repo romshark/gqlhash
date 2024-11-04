c6058e730f6c4dd41cc045329a263b2b68436b51
Roman Sharkov <roman.scharkov@gmail.com>
2024-11-04 01:42:33 +0100

    chore: Bump version to 1.2.1

--- cmd/gqlhash/main.go
+++ cmd/gqlhash/main.go
@@ -27,7 +27,7 @@ import (
 )
 
 const (
-	Version                = `1.2.0`
+	Version                = `1.2.1`
 	SupportedHashFunctions = "sha1, sha2, sha3, md5, blake2b, blake2s, " +
 		"fnv, crc32, crc64"
 	SupportedOutputFormats = "hex, base32, base64"
